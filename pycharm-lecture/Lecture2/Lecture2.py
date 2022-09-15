9b58f85dab3ae07227b4cb3c2aa0d007fac79d67
Gose <97828478+Winter-Climbing@users.noreply.github.com>
2022-09-15 14:45:41 +0900

    3weeks

---
+++ pycharm-lecture/Lecture2/Lecture2.py
@@ -0,0 +1,43 @@
+print('hello world sdfgfgd')
+
+# 수치형 데이터 연습
+
+a = 123
+print(a)
+
+a = -178
+print(a)
+
+b = 1.2
+print(b)
+
+a = 4.24E10
+print(a)
+
+b = 4.24e-10
+print(b)
+
+print(2 ** 3 ** 2)
+
+# 스트링 연습
+
+x = 'life is short \nyon need python\n'
+y = 'javascript is strange \t\t\tyou need typescript'
+z = '\"python is very easy\" he says'
+print(x)
+print(y)
+print(z)
+
+# 스트링 연산
+
+head = 'JS'
+tail = 'is strange'
+print(head +" "+ tail)
+
+mul = 'python'
+print(mul * 4)
+
+leng = 'life is too short'
+print(len(leng))
+len(leng)
+
