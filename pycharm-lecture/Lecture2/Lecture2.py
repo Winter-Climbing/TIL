af68402302ca87d28395496527cf323948995a7b
Gose <97828478+Winter-Climbing@users.noreply.github.com>
2022-09-19 11:46:37 +0900

    Add files via upload

--- pycharm-lecture/Lecture2/Lecture2.py
+++ pycharm-lecture/Lecture2/Lecture2.py
@@ -37,7 +37,39 @@ print(head +" "+ tail)
 mul = 'python'
 print(mul * 4)
 
+# 문자열 길이
 leng = 'life is too short'
 print(len(leng))
 len(leng)
 
+# slice
+s = 'http://www.google.com'
+protocol = s[0:4]
+hostname = s[7:]
+print(protocol +'://'+ hostname)
+
+url = 'https://www.kmu.ac.kr/uni/main/page.jsp?mnu_uid=143&cmd=2&parm_bod_uid=219435'
+protocol2 = url[0:4]
+part = url[7:20]
+path = url[20:]
+print(protocol2 + ':/' + part + path)
+print(path.upper())
+
+# Formatting
+# %d - int
+# %s - string
+# %c - 문자 1개
+# %f - real(실수)
+# %% - Literal % (문자 '%' 자체)
+
+formatting = 'I ate %d apples and %d bananas '%(3, 2)
+print(formatting)
+number1 = 30
+test = 'i ate %d apple' %number1
+print(test)
+
+name1 = '홍길도'
+age = 30
+f'나의 이름은 {name1} 나이는 ${age}'
+
+# 문자열에 관한 함수
