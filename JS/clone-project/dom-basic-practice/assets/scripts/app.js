21652fca74417020c9546ca0de02928d99136aa8
Gose <niabc70@gmail.com>
2022-11-17 17:46:01 +0900

    add movie

--- JS/clone-project/dom-basic-practice/assets/scripts/app.js
+++ JS/clone-project/dom-basic-practice/assets/scripts/app.js
@@ -13,6 +13,50 @@ const submitAddMovieButton = addMovieModal.querySelector(".btn--success");
 
 const userInputs = addMovieModal.querySelectorAll("input");
 
+const entryTextSection = document.getElementById("entry-text");
+
+const movies = [];
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  } else {
+    entryTextSection.style.display = "none";
+  }
+};
+
+const deleteMovieHandler = (movieId) => {
+  let movieIndex = 0;
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      break;
+    }
+    movieIndex++;
+  }
+  movies.splice(movieIndex, 1);
+  const listRoot = document.getElementById("movie-list");
+  listRoot.children[movieIndex].remove();
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
+  const newMovieElement = document.createElement("li");
+  newMovieElement.className = "movie-element";
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+      <img src="${imageUrl}" alt="${title}"/>
+    </div>
+    <div class="movie-element__info">
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+    </div>
+  `;
+
+  newMovieElement.addEventListener("click", deleteMovieHandler.bind(null, id));
+
+  const listRoot = document.getElementById("movie-list");
+  listRoot.append(newMovieElement);
+};
+
 const toggleAddMovie = () => {
   addMovieModal.classList.toggle("visible");
 };
@@ -24,12 +68,19 @@ const toggleBackdrop = () => {
 const toggleMovieModal = () => {
   toggleAddMovie();
   toggleBackdrop();
+  clearMovieInput();
 };
 
 const backdropClickHandler = (e) => {
   if (e.target === changeBackground) {
     toggleAddMovie();
     toggleBackdrop();
+  }
+};
+
+const clearMovieInput = () => {
+  for (const usrInputs of userInputs) {
+    usrInputs.value = "";
   }
 };
 
@@ -49,6 +100,27 @@ const addMovieHandler = (e) => {
     alert("Please enter valid value (빈값 없이 1과 5사이로 쓰셈!)");
     return;
   }
+
+  const newMovie = {
+    // 원래 math.random은 똑같은 숫자가 나올 수 있기 때문에 선택 대상이 아니다.
+    id: Math.random().toString(),
+    title: titleValue,
+    image: imgUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleAddMovie();
+  toggleBackdrop();
+  clearMovieInput();
+  renderNewMovieElement(
+    newMovie.id,
+    newMovie.title,
+    newMovie.image,
+    newMovie.rating
+  );
+  updateUI();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
