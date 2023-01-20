31154f8542dff9609d6ea863e2b2c4c9dce34c66
Gose <niabc70@gmail.com>
2023-01-20 19:58:39 +0900

    section3 2~4

--- JS-Algorithm/js-algorithm/exercise/section-3/1/1.js
+++ JS-Algorithm/js-algorithm/exercise/section-3/1/1.js
@@ -18,7 +18,12 @@ let input = fs.readFileSync(filePath).toString().trim();
 
 function solution(str) {
   let string = str.toUpperCase();
-
+  let len = parseInt(str.length / 2);
+  let answer = "YES";
+  for (let i = 0; i < len; i++) {
+    if (string[i] != string[str.length - i - 1]) return "NO";
+  }
+  return answer;
   return string.split("").reverse().join("") === string ? "YES" : "NO";
 }
 
