853cbc2d779c83b048a502b98fee16fd6d77b3f8
Gose <niabc70@gmail.com>
2023-01-21 16:07:14 +0900

    section3 5, section4 1~2

--- JS-Algorithm/js-algorithm/exercise/section-2/3/3.js
+++ JS-Algorithm/js-algorithm/exercise/section-2/3/3.js
@@ -1,20 +1,3 @@
-// ▣ 입력설명
-// 첫 번째 줄에 게임 횟수인 자연수 N(1<=N<=100)이 주어집니다.
-// 두 번째 줄에는 A가 낸 가위, 바위, 보 정보가 N개 주어집니다.
-// 세 번째 줄에는 B가 낸 가위, 바위, 보 정보가 N개 주어집니다.
-// ▣ 출력설명
-// 각 줄에 각 회의 승자를 출력합니다. 비겼을 경우는 D를 출력합니다.
-// ▣ 입력예제 1
-// 5
-// 2 3 3 1 3
-// 1 1 2 2 3
-// ▣ 출력예제 1
-// A
-// B
-// A
-// B
-// D
-
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
--- JS-Algorithm/js-algorithm/exercise/section-2/6/6.js
+++ JS-Algorithm/js-algorithm/exercise/section-2/6/6.js
@@ -1,19 +1,3 @@
-// 입력설명
-// 첫 줄에 자연수 N이 주어진다.(1<=N<=50)
-// 두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는
-// 다.
-// ▣ 출력설명
-// 최대합을 출력합니다.
-// ▣ 입력예제 1
-// 5
-// 10 13 10 12 15
-// 12 39 30 23 11
-// 11 25 50 53 15
-// 19 27 29 37 27
-// 19 13 30 13 19
-// ▣ 출력예제
-// 155
-
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
