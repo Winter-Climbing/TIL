d4e1c045487d201b1089ac72f0d9c0ab51f4e324
Gose <niabc70@gmail.com>
2023-04-18 10:25:37 +0900

    파라매트릭 2513

---
+++ JS-Algorithm/fastcampus/section5/2512/app.js
@@ -0,0 +1,21 @@
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+// let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+// const arr = input[1]
+//   .split(' ')
+//   .map(Number)
+//   .sort((a, b) => a - b);
+// const maxNum = Number(input[2]);
+// const arrSum = arr.reduce((a, b) => a + b);
+
+// console.log(arrSum);
+
+// let answer = 0;
+
+// if (arrSum <= maxNum) {
+//   answer += arr[arr.length - 1];
+// } else {
+// }
+
+// console.log(answer);
